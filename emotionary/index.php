e11c7b3a3b1ba4e872b6a1815b90cc89ad7b7175
Hain-Lee Hsueh <leehsueh7@gmail.com>
2012-08-22 02:35:45 -0700

    emotionary under construction site

---
+++ emotionary/index.php
@@ -0,0 +1,72 @@
+<?php require_once( '../couch/cms.php' ); ?>
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width" />
+
+	<title>Me.Mu - Social Learning Through Playful Movement</title>
+  
+	<!-- Included CSS Files -->
+	<link rel="stylesheet" href="../stylesheets/foundation.css">
+	<link rel="stylesheet" href="../stylesheets/emotionary.css">
+	<!-- <link rel="stylesheet/less" type="text/css" href="stylesheets/app.less"> -->
+
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="stylesheets/ie.css">
+	<![endif]-->
+	
+	<script src="../javascripts/modernizr.foundation.js"></script>
+	<!--<script type="text/javascript" src="javascripts/less-1.3.0.min.js"></script>-->
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+	<!-- container -->
+	<div class="container">
+		<div class="row banner">
+			<div class="twelve columns centered">
+				<header>
+					<img id="smiley" src="../images/SmileyFace.png" alt="Me.Mu">
+					<p id="memu-text">Me<span>.</span>Mu</p>
+					<p id="introduces-text">introduces...</p>
+					<img src="../images/emotionary/emotionarybanner.png" alt="emotionary">
+
+				</header>
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="twelve columns centered">
+				<p id="coming-soon-text">Coming soon!</p>
+				<p>Emotionary by Me.Mu is an emotion diary tool for iPad. It's designed for therapists to help children and adolescents with autism or similar mental disorders understand emotions in the context of their own lives.</p>
+			</div>
+		</div>
+
+	</div>
+	<!-- container -->
+
+
+
+
+	<!-- Included JS Files -->
+	<script src="../javascripts/jquery.min.js"></script>
+	<script src="../javascripts/foundation.js"></script>
+	<script src="../javascripts/app.js"></script>
+
+</body>
+</html>
+<?php //COUCH::invoke(); ?>
